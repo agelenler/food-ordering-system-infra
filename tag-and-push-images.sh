e9ce42124f2436a3d1a7e0b25ef2f3b123750c15
ali gelenler <you@example.com>
2023-08-30 00:04:55 +0200

    Food ordering system infrastructure

--- tag-and-push-images.sh
+++ tag-and-push-images.sh
@@ -5,7 +5,7 @@ docker tag com.food.ordering.system/payment.service:$1 europe-west4-docker.pkg.d
 docker tag com.food.ordering.system/restaurant.service:$1 europe-west4-docker.pkg.dev/food-ordering-system-346904/food-ordering-system-repository/restaurant.service:$1
 docker tag com.food.ordering.system/customer.service:$1 europe-west4-docker.pkg.dev/food-ordering-system-346904/food-ordering-system-repository/customer.service:$1
 
-docker push europe-west4-docker.pkg.dev/food-ordering-system-346415/food-ordering-system-repo/order.service:$1
+docker push europe-west4-docker.pkg.dev/food-ordering-system-346904/food-ordering-system-repository/order.service:$1
 docker push europe-west4-docker.pkg.dev/food-ordering-system-346904/food-ordering-system-repository/payment.service:$1
 docker push europe-west4-docker.pkg.dev/food-ordering-system-346904/food-ordering-system-repository/restaurant.service:$1
 docker push europe-west4-docker.pkg.dev/food-ordering-system-346904/food-ordering-system-repository/customer.service:$1
